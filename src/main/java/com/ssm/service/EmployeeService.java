a6c404598dc4f31c2f05b5c1717970e9a97c6073
hexiaolong <1355164862@qq.com>
2020-09-18 23:57:10 +0800

    Initial commit

---
+++ src/main/java/com/ssm/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.ssm.service;
+
+import com.ssm.pojo.Employee;
+
+/**
+ * @data 2020/9/18 21:28
+ */
+public interface EmployeeService {
+
+    /**
+     * 插入一条员工信息
+     * @param emp 员工信息
+     * @return
+     */
+    public int insert(Employee emp);
+}
